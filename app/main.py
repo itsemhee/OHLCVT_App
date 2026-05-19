b9efe6849913463b555ce36f5c84740044ce5171
Busha Digital Ltd <bushadigital@Busha-C02DQFWAP3XY.local>
2026-05-19 11:23:57 +0100

    updated file

--- app/main.py
+++ app/main.py
@@ -6,6 +6,14 @@ from app.schemas import OHLCVTCreate, UserCreate
 from app.utils import verify, hash
 from app.oauth2 import create_access_token, verify_access_token, get_current_user
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+import logging
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= "%(asctime)s - %(levelname)s - %(message)s"
+)
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -15,6 +23,8 @@ Base.metadata.create_all(bind=engine)
 @app.post("/users")
 def login_user(user: UserCreate, db: Session =Depends(get_db), 
                get_current_user: int =Depends(get_current_user)):
+    
+    logger.info("Creating new User")
 
     hashed_password = hash(user.password)
     user.password = hashed_password
@@ -24,16 +34,22 @@ def login_user(user: UserCreate, db: Session =Depends(get_db),
     db.commit()
     db.refresh(new_user)
 
+    logger.info(f"User with id {new_user.id}")
+
     return new_user
 
 @app.get("/users/{id}")
 def get_user(id: int, db: Session= Depends(get_db)):
 
+    logger.info(f"Fetching User With id {id}")
+
     user = db.query(User).filter(User.id == id).first()
     
-    if not get_user:
+    if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"User with id: {id} was not found")
+    
+    logger.info(f"User with id {id} fetched successfully")
 
     return user
 
@@ -42,6 +58,8 @@ def get_user(id: int, db: Session= Depends(get_db)):
 def login(user_credentials: OAuth2PasswordRequestForm= Depends(), 
           db: Session = Depends(get_db)):
     
+    logger.info(f"Login attempt for {user_credentials.username}")
+
     {
         "username": "asfjn",
         "password": "yjrderdtfygk"
@@ -59,6 +77,7 @@ def login(user_credentials: OAuth2PasswordRequestForm= Depends(),
     
     access_token = create_access_token(data= {"user_id": user.id})
 
+    logger.info(f"User {user.id} logged in successfully")
 
     return{"access_token": access_token, "token_type": "bearer"}
 
